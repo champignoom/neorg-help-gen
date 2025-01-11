104ba2edb049f3eed3d3fe2b5ac0f18cc43acb51
champignoom <champignoom@gmail.com>
2025-01-12 03:52:13 +0800

    minor fix

--- all.sh
+++ all.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+
+DIR="$(dirname "$0")"
+
 mkdir norg
 
 for i in *.md; do
-	echo $i; pandoc $i -t markdown  --lua-filter=filter.lua --lua-filter=filter2.lua -t json | /home/exp/code/neorg-gen-norg-help/minorg/minorg generate -f -o norgs/${i%.md}.norg
+	echo $i; pandoc $i -t markdown  --lua-filter="$DIR/filter.lua" --lua-filter="$DIR/filter2.lua" -t json | /home/exp/code/neorg-gen-norg-help/minorg/minorg generate -f -o norgs/${i%.md}.norg
 done
 
 for i in norgs/*.norg; do
-	echo $i; nvim $i --headless -c 'luafile all.lua'
-	sed -i 's@^[*]\+ .* (empty$@& list)@; s@^\( *-\+\) (@\1 \\(@' "$i"
+	echo $i; nvim $i --headless -c "luafile $DIR/all.lua"
+	sed -i 's@^[*]\+ .* (empty$@& list)@; s@^\( *-\+\) (@\1 \\(@; s@{https://github.com/nvim-neorg/neorg/wiki/\([A-Za-z-]\+\)}@{:\1:}@g; s@{\(Dependencies\)}@{:\1:}@' "$i"
 done
 
-sed -i 's@{https://github.com/nvim-neorg/neorg/wiki/\([A-Za-z-]\+\)}@{:\1:}@g; s@{\(Dependencies\)}@{:\1:}@' norgs/Home.norg
+rm norgs/_Sidebar.norg
+
